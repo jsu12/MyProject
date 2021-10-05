511cb76140df6a6a2dba3dd3c439746419179243
DESKTOP-SCT93SA\User <rhtth123@gmail.com>
2021-10-05 12:28:31 +0900

    Translation Project

---
+++ app/src/main/java/com/rhtth/myproject/Login/Validation.kt
@@ -0,0 +1,19 @@
+package com.rhtth.myproject.Login
+
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
+fun isEmail(email: String?) : Boolean {
+
+       var returnValue = false
+       val regex = "^[_a-zA-Z0-9-\\.]+@[\\.a-zA-Z0-9-]+\\.[a-zA-Z]+$"
+       val p = Pattern.compile(regex)
+       val m: Matcher = p.matcher(email)
+       if (m.matches()) {
+            returnValue = true
+       }
+
+       return returnValue
+}
